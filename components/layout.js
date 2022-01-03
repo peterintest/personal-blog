9f1428f2c4321e8ed83ea4c893879d5f40639699
Peter <peterjohnson1990@gmail.com>
2022-01-03 12:04:00 +0000

    Initial commit from Create Next App

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Peter Johnson'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Peter Johnson'
 
 export default function Layout({ children, home }) {
   return (
