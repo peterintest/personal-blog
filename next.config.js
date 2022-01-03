80781d84afe8d7c5f14511bcc005cf7738519535
Peter <peterjohnson1990@gmail.com>
2022-01-03 12:04:00 +0000

    Initial commit from Create Next App

--- next.config.js
+++ next.config.js
@@ -3,3 +3,9 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   assetPrefix: isProd ? "/personal-blog/" : "",
 };
+
+const withOptimizedImages = require('next-optimized-images')
+
+module.exports = withOptimizedImages({
+    handleImages: ['jpeg', 'png', 'svg'],
+})
